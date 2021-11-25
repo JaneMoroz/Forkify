8ec34f87542fa9781dc5f4a9edb47bd9bbd228f2
Jane Moroz <Moroz.jane@yahoo.com>
2021-11-25 19:14:26 +0300

    Enabled Uploading a New Recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '621c0e36-cee5-4e08-b453-01b6890b5a2b';
+export const MODAL_CLOSE_SEC = 2.5;
